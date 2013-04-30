344ce5a5a248a6249fc3bfa0cea290da06dc8754
John Mark Schofield <jms@schof.org>
2013-04-30 16:04:50 -0700

    More debug lines.

--- experiments/counters.py
+++ experiments/counters.py
@@ -4,7 +4,7 @@ import redis
 from redis.exceptions import ConnectionError, ResponseError
 
 import logging
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 logger.debug('Loading counters.py')
 print('debug version of loading counters.py')
 
