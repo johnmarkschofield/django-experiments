fb7e1ca1134b2e22e92122f4e942d8250d81a4fd
John Mark Schofield <jms@schof.org>
2013-04-30 18:30:11 -0700

    More debug logging.

--- experiments/manager.py
+++ experiments/manager.py
@@ -1,6 +1,14 @@
+import logging
+
 from django.conf import settings
-from experiments.models import Experiment
+
 from modeldict import ModelDict
+from djangofu.general import get_env_variable
 
+from experiments.models import Experiment
 
+logger = logging.getLogger(get_env_variable('LOGROOT') + '.' + __name__)
+
+logger.debug('Creating experiment_manager')
 experiment_manager = ModelDict(Experiment, key='name', value='value', instances=True, auto_create=getattr(settings, 'EXPERIMENTS_AUTO_CREATE', True))
+logger.debug('Done creating experiment_manager')
