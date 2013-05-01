8cdeb76b4ef3e00982ce0947e51446c84c66f385
John Mark Schofield <jms@schof.org>
2013-05-01 14:43:57 -0700

    Fixing logging; less ugly and more production-ready.

--- experiments/counters.py
+++ experiments/counters.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.conf import settings
 
 import redis
@@ -5,9 +7,7 @@ from redis.exceptions import ConnectionError, ResponseError
 
 from djangofu.general import get_env_variable
 
-import logging
 logger = logging.getLogger(get_env_variable('LOGROOT') + '.' + __name__)
-logger.debug('Loading counters.py')
 
 REDIS_HOST = getattr(settings, 'EXPERIMENTS_REDIS_HOST', 'localhost')
 REDIS_PORT = getattr(settings, 'EXPERIMENTS_REDIS_PORT', 6379)
@@ -20,6 +20,7 @@ logger.debug('Done connecting to redis.')
 
 COUNTER_CACHE_KEY = 'experiments:participants:%s'
 COUNTER_FREQ_CACHE_KEY = 'experiments:freq:%s'
+
 
 def increment(key, participant_identifier, count=1):
     logger.debug('incrementing a record')
@@ -37,8 +38,9 @@ def increment(key, participant_identifier, count=1):
         r.hincrby(freq_cache_key, new_value, 1)
     except (ConnectionError, ResponseError):
         # Handle Redis failures gracefully
-        logger.debug('django-experiments failure 1')
+        logger.critical('Unable to increment record in Redis')
         pass
+
 
 def clear(key, participant_identifier):
     try:
@@ -51,18 +53,21 @@ def clear(key, participant_identifier):
         freq_cache_key = COUNTER_FREQ_CACHE_KEY % key
         r.hincrby(freq_cache_key, freq, -1)
     except (ConnectionError, ResponseError):
-        logger.debug('django-experiments failure 2')
         # Handle Redis failures gracefully
+        logger.critical('Unable to clear record in Redis')
         pass
 
+
 def get(key):
+    logger.debug('Retrieving key %s from Redis' % key)
     try:
         cache_key = COUNTER_CACHE_KEY % key
         return r.hlen(cache_key)
     except (ConnectionError, ResponseError):
-        logger.debug('django-experiments failure 3')
         # Handle Redis failures gracefully
+        logger.critical('Unable to get key %s from Redis' % key)
         return 0
+
 
 def get_frequency(key, participant_identifier):
     try:
@@ -70,9 +75,10 @@ def get_frequency(key, participant_identifier):
         freq = r.hget(cache_key, participant_identifier)
         return int(freq) if freq else 0
     except (ConnectionError, ResponseError):
-        logger.debug('django-experiments failure 4')
         # Handle Redis failures gracefully
+        logger.critical('Unable to getfrequency for key %s' % key)
         return 0
+
 
 def get_frequencies(key):
     try:
@@ -81,11 +87,12 @@ def get_frequencies(key):
         # briefly be a negative result for some frequency count. We discard these
         # as they shouldn't really affect the result, and they are about to become
         # zero anyway.
-        return dict((int(k),int(v)) for (k,v) in r.hgetall(freq_cache_key).items() if int(v) > 0)
+        return dict((int(k), int(v)) for (k, v) in r.hgetall(freq_cache_key).items() if int(v) > 0)
     except (ConnectionError, ResponseError):
-        logger.debug('django-experiments failure 5')
         # Handle Redis failures gracefully
+        logger.critical('Unable to get_frequencies for key %s' % key)
         return tuple()
+
 
 def reset(key):
     try:
@@ -95,9 +102,10 @@ def reset(key):
         r.delete(freq_cache_key)
         return True
     except (ConnectionError, ResponseError):
-        logger.debug('django-experiments failure 6')
         # Handle Redis failures gracefully
+        logger.critical('Unable to reset key %s' % key)
         return False
+
 
 def reset_pattern(pattern_key):
     #similar to above, but can pass pattern as arg instead
@@ -110,6 +118,6 @@ def reset_pattern(pattern_key):
             r.delete(key)
         return True
     except (ConnectionError, ResponseError):
-        logger.debug('django-experiments failure 7')
         # Handle Redis failures gracefully
+        logger.critical('Unable to reset_pattern for pattern_key %s' % pattern_key)
         return False
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 import re
 
+
 def parse_requirements(file_name):
     requirements = []
     for line in open(file_name, 'r').read().split('\n'):
@@ -16,6 +17,7 @@ def parse_requirements(file_name):
 
     return requirements
 
+
 def parse_dependency_links(file_name):
     dependency_links = []
     for line in open(file_name, 'r').read().split('\n'):
@@ -26,7 +28,7 @@ def parse_dependency_links(file_name):
 
 
 setup(name='django-experiments',
-      version='0.3.5a2',
+      version='0.3.5b',
       description='Python Django AB Testing Framework',
       author='Chris Villa',
       author_email='chris@mixcloud.com',
@@ -34,7 +36,7 @@ setup(name='django-experiments',
       packages=find_packages(exclude=["example_project"]),
       include_package_data=True,
       license="MIT license, see LICENSE file",
-      install_requires = parse_requirements('requirements.txt'),
-      dependency_links = parse_dependency_links('requirements.txt'),
+      install_requires=parse_requirements('requirements.txt'),
+      dependency_links=parse_dependency_links('requirements.txt'),
       long_description=open('README.rst').read(),
-)
+      )
