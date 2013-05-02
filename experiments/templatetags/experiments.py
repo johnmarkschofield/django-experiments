ace9c2f4004c5e03b43a352b073c5d5e91b87a9c
John Mark Schofield <jms@schof.org>
2013-05-02 09:14:08 -0700

    Modifications for PEP8 compliance.

--- experiments/templatetags/experiments.py
+++ experiments/templatetags/experiments.py
@@ -1,17 +1,19 @@
 from __future__ import absolute_import
+from uuid import uuid4
 
 from django import template
 from django.core.urlresolvers import reverse
 
 from experiments.utils import participant
 from experiments.manager import experiment_manager
-from uuid import uuid4
 
 register = template.Library()
 
+
 @register.inclusion_tag('experiments/goal.html')
 def experiment_goal(goal_name):
-    return { 'url': reverse('experiment_goal', kwargs={'goal_name': goal_name, 'cache_buster': uuid4()}) }
+    return {'url': reverse('experiment_goal', kwargs={'goal_name': goal_name, 'cache_buster': uuid4()})}
+
 
 class ExperimentNode(template.Node):
     def __init__(self, node_list, experiment_name, alternative, user_variable):
@@ -38,6 +40,7 @@ class ExperimentNode(template.Node):
             response = ""
 
         return response
+
 
 def _parse_token_contents(token_contents):
     (_, experiment_name, alternative), remaining_tokens = token_contents[:3], token_contents[3:]
@@ -66,11 +69,11 @@ def _parse_token_contents(token_contents):
 def experiment(parser, token):
     """
     Split Testing experiment tag has the following syntax :
-    
+
     {% experiment <experiment_name> <alternative>  %}
     experiment content goes here
     {% endexperiment %}
-    
+
     If the alternative name is neither 'test' nor 'control' an exception is raised
     during rendering.
     """
@@ -81,14 +84,15 @@ def experiment(parser, token):
         node_list = parser.parse(('endexperiment', ))
         parser.delete_first_token()
     except ValueError:
-        raise template.TemplateSyntaxError("Syntax should be like :"
-                "{% experiment experiment_name alternative [weight=val] [user=val] %}")
+        raise template.TemplateSyntaxError(
+            "Syntax should be like :{% experiment experiment_name alternative [weight=val] [user=val] %}")
 
     experiment = experiment_manager.get(experiment_name, None)
     if experiment:
         experiment.ensure_alternative_exists(alternative, weight)
 
     return ExperimentNode(node_list, experiment_name, alternative, user_variable)
+
 
 @register.simple_tag(takes_context=True)
 def visit(context):
